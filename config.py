50a9722fb567801c97361b6e8fde3995c103cee6
chenglongfu11 <f951117c@icloud.com>
2020-05-12 11:22:42 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,20 @@
+#Configuration file
+"""
+    TO RUN : add your own IDA ICE application path and building path
+"""
+
+
+APP_PATH = "D:\\idaice\\bin\\"
+#
+#
+BUILDING_PATH = "d:\\ide_mine\\changing\\ut1_7floorwithWin.idm"
+# BUILDING_PATH = [b"d:\\ide_mine\\changing\\ut1.idm", b"d:\\ide_mine\\changing\\ut1_7floor.idm", b"d:\\ide_mine\\changing\\ut1_7floorwithWin.idm"]
+
+
+#Mysql config info
+MYSQL_HOST = 'localhost'
+MYSQL_DBNAME = 'testdb'
+MYSQL_USER = 'root'
+MYSQL_PASSWD = 'password'
+
+MYSQL_PORT = 3306
